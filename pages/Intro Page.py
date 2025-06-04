9ff0733997169bf3bbf1e68616ae85debcc6b36e
HoTruongAnBBA2024 <106240001@student.vgu.edu.vn>
2025-06-04 17:26:15 +0700

    Update Intro Page.py

--- pages/Intro Page.py
+++ pages/Intro Page.py
@@ -79,4 +79,4 @@ st.markdown("""
 
 # === Nút chuyển hướng đến tab chính (HomePage) ===
 if st.button("🚀 Explore Now"):
-    st.switch_page("pages/1_HomePage.py")
+    st.switch_page("pages/01_HomePage.py")
