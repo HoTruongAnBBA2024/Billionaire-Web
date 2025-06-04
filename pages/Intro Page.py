569cb5bb7085c59f1c26e0d42954356efc8fb339
HoTruongAnBBA2024 <106240001@student.vgu.edu.vn>
2025-06-04 17:40:44 +0700

    Update Intro Page.py

--- pages/Intro Page.py
+++ pages/Intro Page.py
@@ -79,4 +79,4 @@ st.markdown("""
 
 # === Nút chuyển hướng đến tab chính (HomePage) ===
 if st.button("🚀 Explore Now"):
-    st.switch_page("Business IT 2 — Python Project")
+    st.switch_page("01_Home Page")
