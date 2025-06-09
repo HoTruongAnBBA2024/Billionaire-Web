456ebac0c1150ab9d306bd4f49b87158fc7823a4
HoTruongAnBBA2024 <106240001@student.vgu.edu.vn>
2025-06-09 10:01:41 +0700

    Update 01_Home Page.py

--- pages/01_Home Page.py
+++ pages/01_Home Page.py
@@ -31,7 +31,7 @@ st.markdown("---")
 colored_header(label="Team Leader & Members", description="Our visuals, powered by passion", color_name="light-blue-70")
 
 
-st.markdown("### 💼 Meet the Team")
+st.markdown("###Meet the Team")
 members = [
    ("Hồ Trường An", "106240001", "image/4.png"),
    ("Đỗ Lê Thái Khang", "106240444", "image/1.png"),
